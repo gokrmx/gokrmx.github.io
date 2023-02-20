f7fb33a8ea51de4726394fe5c2875a5276b930a9
90k <56047586+gokrmx@users.noreply.github.com>
2023-02-19 20:35:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
+
 var menu = document.getElementById("menu");
 var ulist = document.getElementById("ulist");
 
